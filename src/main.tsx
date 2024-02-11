9b63be42534e08220f339998614e130345053d74
Shevchenko Andrey <demorest49de@gmail.com>
2024-02-11 18:28:59 +0300

    Changes App.module.css main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,8 @@ import picture from"./Assets/img/premium_photo-1669324357471-e33e71e3f3d8.avif"
 export const Main = () => {
     return (
         <main className={s.content}>
-            <img src={picture} alt="picture" style={{objectFit: "cover", width: "70%"}} />
+            {/*<img src={picture} alt="picture" style={{objectFit: "cover", width: "100%"}} />*/}
+            <div style={{width: "100%", backgroundColor: 'beige', height: '170px', padding: '10px'}}>hello</div>
         </main>
     );
 };
